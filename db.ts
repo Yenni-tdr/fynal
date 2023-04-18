9eef404136e52e536444ce98a1c8ed12fd36b5e2
Yenni-tdr <83454001+Yenni-tdr@users.noreply.github.com>
2023-04-18 13:41:53 +0200

    Update db.ts

--- db.ts
+++ db.ts
@@ -8,6 +8,7 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ['query'],
+    
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
