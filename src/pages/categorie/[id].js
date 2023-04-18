26ee2584f9afe3285c7a26d1b6b7d1930b83e7f0
Yenni-tdr <83454001+Yenni-tdr@users.noreply.github.com>
2023-04-18 13:53:46 +0200

    Add files via upload

--- src/pages/categorie/[id].js
+++ src/pages/categorie/[id].js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getAllCategoriesID, getCategorieProductsData } from '../../fonctions/categorie';
 import { getCategorieIdData } from '../../fonctions/SidebarData';
+import * as FaIcons from 'react-icons/fa';
 
 import {
     arrayUnique,
@@ -156,7 +157,6 @@ export default function Categorie({ catData }) {
     - delaisLivraison : faire un slider (si possible, sinon un menu deroulant), directement regarder dans le tableau 'produits', pas besoin de récupérer d'information au préalable
     
     - hauteur/longueur/largeur/poids : pas de filtre nécessaire, surtout utile pour le colis
-
     - categorie : cases à cocher '[case] Nom catégorie', comparer avec les valeurs dans le tableau, récupérer au préalable les catégories des produits affichés sur la page (donc afficher ce filtre seulement si l'utilisateur fait une recherche)
     
     - vendeur : case à cocher, comparer avec les valeurs dans le tableau, récupérer tous les vendeurs des produits affichés au préalable
@@ -166,17 +166,19 @@ export default function Categorie({ catData }) {
         <div>
             <h1 className='text-center mt-8 font-semibold text-3xl italic'>{ infosCategorie.libelle }</h1>
             <h2 className='text-center mt-8 font-semibold text-xl italic'>{ infosCategorie.description }</h2>
-            <button onClick={() => handleBouton(FILTRE_PRIX_CROISSANT_STRING)}>{FILTRE_PRIX_CROISSANT_STRING}</button>
-            <br />
-            <button onClick={() => handleBouton(FILTRE_PRIX_DECROISSANT_STRING)}>{FILTRE_PRIX_DECROISSANT_STRING}</button>
-            <br />
-            <button onClick={() => handleBouton(FILTRE_ALPHABETIQUE_CROISSANT_STRING)}>{FILTRE_ALPHABETIQUE_CROISSANT_STRING}</button>
-            <br />
-            <button onClick={() => handleBouton(FILTRE_ALPHABETIQUE_DECROISSANT_STRING)}>{FILTRE_ALPHABETIQUE_DECROISSANT_STRING}</button>
-            <br />
-            <input type='checkbox' id='stockCheckbox' onClick={(eventCheckbox) => handleBouton(FILTRE_STOCK, eventCheckbox)}></input>{FILTRE_STOCK}
-            <br />
-            <input type='checkbox' id='reductionCheckbox' onClick={(eventCheckbox) => handleBouton(FILTRE_REDUCTION, eventCheckbox)}></input>{FILTRE_REDUCTION}
+            <div className='flex justify-end gap-4 mr-10'>
+                <select className="select select-bordered w-full max-w-xs">
+                    <option disabled selected>Trier par :</option>
+                    <option onSelect={() => handleBouton(FILTRE_PRIX_CROISSANT_STRING)}>{FILTRE_PRIX_CROISSANT_STRING}</option>
+                    <option onSelect={() => handleBouton(FILTRE_PRIX_DECROISSANT_STRING)}>{FILTRE_PRIX_DECROISSANT_STRING}</option>
+                    <option onSelect={() => handleBouton(FILTRE_ALPHABETIQUE_CROISSANT_STRING)}>{FILTRE_ALPHABETIQUE_CROISSANT_STRING}</option>
+                    <option onSelect={() => handleBouton(FILTRE_ALPHABETIQUE_DECROISSANT_STRING)}>{FILTRE_ALPHABETIQUE_DECROISSANT_STRING}</option>
+                </select>
+                <div className='flex flex-col gap-1'>
+                    <span><input type="checkbox" id='stockCheckbox' className="checkbox" onClick={(eventCheckbox) => handleBouton(FILTRE_STOCK, eventCheckbox)}/> {FILTRE_STOCK}</span>
+                    <span><input type='checkbox' id='reductionCheckbox' className="checkbox" onClick={(eventCheckbox) => handleBouton(FILTRE_REDUCTION, eventCheckbox)}/> {FILTRE_REDUCTION}</span>
+                </div>
+            </div>
             <div className="bg-white">
                 <div className="mx-auto max-w-2xl -mt-16 px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                     <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
@@ -197,9 +199,9 @@ export default function Categorie({ catData }) {
                                         <h3 className="mt-4 text-sm text-gray-700">{produit.nom}</h3>
                                         <p className="mt-1 text-lg font-medium text-gray-900">{produit.prix}</p>
                                     </div>
-                                    {/* <button id='addCart' className="bg-slate-200 hover:bg-slate-300 text-black  text-xl font-semibold py-2 px-4 mt-4 rounded shadow"
+                                    { <button id='addCart' className="bg-slate-200 hover:bg-slate-300 text-black  text-xl font-semibold py-2 px-4 mt-4 rounded shadow"
                                             onClick={() => addToCart(produit)}> <FaIcons.FaCartPlus/>
-                                    </button> */}
+                                    </button> }
                                 </div>
                                 </a>
                             );
