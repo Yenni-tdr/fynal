ec584f35063c8af6f76c17562895752379666f9c
Nino Hamel <Nino.Hamel@Yahoo.fr>
2023-04-20 00:11:53 +0200

    Add files via upload

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { getCategorieIdData } from '../../fonctions/SidebarData';   
 import { prisma } from "../../../db";
 
 export async function getStaticPaths(){
@@ -25,6 +26,7 @@ export async function getStaticPaths(){
 export async function getStaticProps(context){
 
     const id = context.params.slug;
+    const categoriesSideMenu = await getCategorieIdData();
     const prodData = await prisma.produit.findMany({
         where: {
             idProduit: parseInt(id),
@@ -33,6 +35,7 @@ export async function getStaticProps(context){
     return {
         props : {
             produit: prodData,
+            categoriesSideMenu,
         },
     };
 
@@ -41,27 +44,27 @@ export async function getStaticProps(context){
 export default function Products({produit}) {
     return (
         <div>                             
-            <section class="text-gray-600 body-font overflow-hidden">
-                <div class="container px-5 py-24 mx-auto">
-                    <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <section className="text-gray-600 body-font overflow-hidden">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="lg:w-4/5 mx-auto flex flex-wrap">
                     
                    
-                    <Image src={produit[0].image} alt={produit[0].nom} class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded">
+                    <img src={produit[0].image} alt={produit[0].nom} className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded">
 
-                    </Image>
-                    <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                        <h2 class="text-sm title-font text-gray-500 tracking-widest">Fynal</h2>
-                        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{produit[0].nom}</h1>
-                        <div class="flex mb-4">
+                    </img>
+                    <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                        <h2 className="text-sm title-font text-gray-500 tracking-widest">Fynal</h2>
+                        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{produit[0].nom}</h1>
+                        <div className="flex mb-4">
                                          
                         </div>
-                        <p class="leading-relaxed">{produit[0].description}</p>
-                        <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">      
+                        <p className="leading-relaxed">{produit[0].description}</p>
+                        <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">      
                         <p>Délai de livraison estimé : {produit[0].delaisLivraison} jours</p>                        
                         </div>
-                        <div class="flex">
-                        <span class="title-font font-medium text-2xl text-gray-900">{produit[0].prix > 0 ? produit[0].prix +" €" : "Rupture de stock"}</span>
-                        <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Ajouter au panier</button>
+                        <div className="flex">
+                        <span className="title-font font-medium text-2xl text-gray-900">{produit[0].prix > 0 ? produit[0].prix +" €" : "Rupture de stock"}</span>
+                        <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Ajouter au panier</button>
                         </div>
                     </div>
                     </div>
