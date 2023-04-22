4865876eb54992c3e65174b89572d9ac77b30e10
Lucas <104208703+Venkkay@users.noreply.github.com>
2023-04-22 19:29:36 +0200

    Add files via upload

--- src/components/AccountMenu.js
+++ src/components/AccountMenu.js
@@ -71,77 +71,12 @@ export default function AccountMenu(){
             </>
         )
     }
-
     else{
-        /*
         return (
             <>
-                {user.status === 0 &&
-                    <>
-                        <li><Link href="" className="justify-between">Historique</Link></li>
-                    </>
-                }
-                {user.status === 1 &&
-                    <>
-                        <li><Link href="/" className="justify-between">Dashboard Vendeur</Link></li>
-                    </>
-                }
-                {user.status === 2 &&
-                    <>
-                        <li><Link href="/" className="justify-between">Dashboard Livreur</Link></li>
-                    </>
-                }
-                {user.status === 3 &&
-                    <>
-                        <li><Link href="/" className="justify-between">Dashboard Admin</Link></li>
-                    </>
-                }
+                <p>Il y un problème, déconnectez-vous et reconnectez vous</p>
                 <li><button onClick={() => removeCookieUser()}>Deconnexion</button></li>
             </>
         )
-
-         */
     }
-
-    /*
-    return (
-        <>
-            {!user &&
-                <>
-                    <li><Link href="/signin" className="justify-between">Connexion</Link></li>
-                    <li><Link href="/register">Inscription</Link></li>
-                </>
-            }
-            {user &&
-                <>
-                    <li><Link href="/account" className="justify-between">Espace utilisateur</Link></li>
-                    {user?.status === 0 &&
-                        <>
-                            <li><Link href="" className="justify-between">Historique</Link></li>
-                        </>
-                    }
-                    {user?.status === 1 &&
-                        <>
-                            <li><Link href="/" className="justify-between">Dashboard Vendeur</Link></li>
-                        </>
-                    }
-                    {user?.status === 2 &&
-                        <>
-                            <li><Link href="/" className="justify-between">Dashboard Livreur</Link></li>
-                        </>
-                    }
-                    {user?.status === 3 &&
-                        <>
-                            <li><Link href="/" className="justify-between">Dashboard Admin</Link></li>
-                        </>
-                    }
-                    <li><button onClick={() => removeCookieUser()}>Deconnexion</button></li>
-                </>
-            }
-
-
-        </>
-    )
-
-     */
 }
