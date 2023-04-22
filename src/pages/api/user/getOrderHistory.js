00aea1315b7f1922b1b28e41d31f3c337e8a56b6
Lucas <104208703+Venkkay@users.noreply.github.com>
2023-04-22 21:49:38 +0200

    Add files via upload

---
+++ src/pages/api/user/getOrderHistory.js
@@ -0,0 +1,55 @@
+import {prisma} from "../../../../db";
+
+
+export default async function handler(req, res){
+    const Utilisateur = prisma.utilisateur;
+
+    const {userId} = req.body;
+
+    try {
+        const utilisateur = await prisma.utilisateur.findUnique({
+            where: {
+                idUtilisateur: userId,
+            },
+            include: {
+                Commande: {
+                    include: {
+                        PanierProduit: {
+                            include: {
+                                Produit: true,
+                            },
+                        },
+                    },
+                },
+            },
+        });
+
+
+        /*
+        const commandes = await prisma.commande.findMany({
+            where:{
+                Utilisateur: {
+                    idUtilisateur: userId
+                }
+            },
+            include:{
+                PanierProduit:{
+                    select:{
+                        quantite: true,
+                        Produit: true
+                    }
+                }
+            }
+        })
+         */
+
+        const commandes = utilisateur.Commande;
+
+        console.log(commandes)
+
+        return res.status(200).json(commandes)
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message: "Une erreur est survenue"})
+    }
+}
