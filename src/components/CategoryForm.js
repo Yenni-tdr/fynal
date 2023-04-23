a21c6c9ed7aa5e6620bcf55a0317f03d6d12f818
François <81768287+guillermfr@users.noreply.github.com>
2023-04-23 19:07:34 +0200

    Added category form component

---
+++ src/components/CategoryForm.js
@@ -0,0 +1,36 @@
+import { ErrorDiv } from "./ErrorDiv"
+
+const formFields = [
+    { labelString: "Libellé* :", id: "libelle" },
+    { labelString: "Description* :", id: "description" },
+]
+
+export const CategoryForm = ({handleSubmit, buttonState, errors}) => {
+    return (
+        <div className="flex items-center justify-center">
+            <form onSubmit={handleSubmit} className="w-full max-w-sm mt-5">
+                { formFields.map((formField) => {
+                    return (
+                        <div key={formField.id} className="mb-6">
+                            <div className="md:flex md:items-center">
+                                <div className="md:w-1/3">
+                                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor={formField.id}>{formField.labelString}</label>
+                                </div>
+                                <div className="md:w-2/3">
+                                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" id={formField.id}></input>
+                                </div>
+                            </div>
+                            <ErrorDiv condition={errors[formField.id.toString()].toString()}>Ce champs n'est pas valide.</ErrorDiv>
+                        </div>
+                    );
+                }) }
+                <div className="md:flex md:items-center mb-20">
+                    <div className="md:w-1/3"></div>
+                    <div className="md:w-2/3">
+                        <button type="submit" disabled={buttonState} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">Ajouter</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
