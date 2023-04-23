edbd1c0b257f21744ef7252665bc05f2102581af
François <81768287+guillermfr@users.noreply.github.com>
2023-04-23 16:31:51 +0200

    Small update

--- src/pages/categorie/[id].js
+++ src/pages/categorie/[id].js
@@ -202,12 +202,6 @@ export default function Categorie({ catData, InitialCart }) {
         }
     }
 
-    /*
-    Gestion des filtres :
-    - delaisLivraison : faire un slider (si possible, sinon un menu deroulant), directement regarder dans le tableau 'produits', pas besoin de récupérer d'information au préalable
-    - categorie : cases à cocher '[case] Nom catégorie', comparer avec les valeurs dans le tableau, récupérer au préalable les catégories des produits affichés sur la page (donc afficher ce filtre seulement si l'utilisateur fait une recherche)
-    */
-
     // Affichage de la page
     return (
         <div>
@@ -251,46 +245,12 @@ export default function Categorie({ catData, InitialCart }) {
                         {produitsTries.map((produit) => {
                             return (
 
-                                // <div className='group' key={produit.idProduit}>
-                                //     <Link
-                                //         href={`/products/${produit.idProduit}`}
-                                //         key={produit.idProduit}
-                                //     >
-                                //         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                                //             <img
-                                //             src={produit.image}
-                                //             alt={produit.nom}
-                                //             height={10}
-                                //             width={10}
-                                //             className="h-full w-full object-cover object-center group-hover:opacity-75"
-                                //             />
-                                //         </div>
-                                //     </Link>
-                                //     <div className='flex flex-row'>
-                                //         <div>
-                                //         <h3 className="mt-4 text-sm text-gray-700">
-                                //             {produit.nom}
-                                //         </h3>
-                                //         <p className="mt-1 text-lg font-medium text-gray-900">
-                                //             {produit.prix} €
-                                //         </p>
-                                //         </div>
-                                //         <button className=" mt-7 text-normal px-4 py-2 ml-auto text-white  bg-stone-800 hover:bg-stone-950 rounded-lg transition ease-in duration-200 focus:outline-none"
-                                //             onClick={async (e) => {
-                                //                 try {
-                                //                 await handleNewCartData(produit, cart);
-                                //                 e.target.reset();
-                                //                 } catch (err) {
-                                //                 console.log(err);
-                                //                 }
-                                //             }}
-                                //         >
-                                //             Ajouter au panier
-                                //         </button>
-                                //     </div>
-                                // </div>
-
-                                <ProductCard key={produit.idProduit} produit={produit} cart={cart} handleNewCartData={handleNewCartData} ></ProductCard>
+                                <ProductCard 
+                                    key={produit.idProduit} 
+                                    produit={produit} 
+                                    cart={cart} 
+                                    handleNewCartData={handleNewCartData}>
+                                </ProductCard>
 
                             );
                         })}
