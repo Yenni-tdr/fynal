46c2f435158dbd9a49ccbb275b5dc52e225da900
Yenni-tdr <83454001+Yenni-tdr@users.noreply.github.com>
2023-04-23 21:28:23 +0200

    Update history.js

--- src/pages/history.js
+++ src/pages/history.js
@@ -1,6 +1,6 @@
 import NavAccount from "../components/NavAccount";
 import HistoryBody from "../components/HistoryBody";
-import { getCategorieIdData } from "../fonctions/SiebarData";
+import { getCategorieIdData } from "../fonctions/SidebarData";
 
 export async function getStaticProps() {
   const categoriesSideMenu = await getCategorieIdData();
