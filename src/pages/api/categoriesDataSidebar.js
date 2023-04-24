772887637540bbbc29809bb06dd34fb13c4c8159
François <81768287+guillermfr@users.noreply.github.com>
2023-04-25 00:39:21 +0200

    Add files via upload

---
+++ src/pages/api/categoriesDataSidebar.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+    const categories = await prisma.categorie.findMany();
+
+    const sidebarCategories = categories.map((categorie) => {
+        return({
+            title: categorie.libelle,
+            path: '/categorie/'.concat(categorie.idCategorie),
+        })
+    });
+
+    res.status(200).json(sidebarCategories);
+}
