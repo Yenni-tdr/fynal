6fe2b88e02a81a0378df75da651ec4eb450d8b98
François <81768287+guillermfr@users.noreply.github.com>
2023-04-25 13:25:45 +0200

    Update account.js

--- src/pages/account.js
+++ src/pages/account.js
@@ -2,16 +2,6 @@ import NavAccount from '../components/NavAccount'
 import RegisterForm from '../components/RegisterForm'
 import AccountBody from "../components/AccountBody";
 
-import { getCategorieIdData } from '../fonctions/SidebarData';
-export async function getStaticProps() {
-    const categoriesSideMenu = await getCategorieIdData();
-    return {
-        props: {
-            categoriesSideMenu,
-        },
-    };
-}
-
 export default function AccountPage(){
     return (
         <main className="flex flex-row flex-1">
