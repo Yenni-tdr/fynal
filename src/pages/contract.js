33ca3543613756d34fa48c84ae91548139d16f8f
François <81768287+guillermfr@users.noreply.github.com>
2023-04-25 13:26:45 +0200

    Update contract.js

--- src/pages/contract.js
+++ src/pages/contract.js
@@ -3,17 +3,6 @@ import ContractBody from "../components/ContractBody";
 import {useCookies} from "react-cookie";
 import ContractAdminBody from "../components/ContractAdminBody";
 import {useEffect, useState} from "react";
-import { getCategorieIdData } from "../fonctions/SidebarData";
-
-export async function getStaticProps() {
-    const categoriesSideMenu = await getCategorieIdData();
-
-    return {
-        props: {
-            categoriesSideMenu: categoriesSideMenu,
-        }
-    }
-}
 
 const Contract = () => {
     return (
