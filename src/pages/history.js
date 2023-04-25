e2d180f38d0160c76e7f0fcb22cb9711ef34851c
François <81768287+guillermfr@users.noreply.github.com>
2023-04-25 13:27:24 +0200

    Update history.js

--- src/pages/history.js
+++ src/pages/history.js
@@ -1,15 +1,5 @@
 import NavAccount from "../components/NavAccount";
 import HistoryBody from "../components/HistoryBody";
-import { getCategorieIdData } from "../fonctions/SidebarData";
-
-export async function getStaticProps() {
-  const categoriesSideMenu = await getCategorieIdData();
-  return {
-    props: {
-      categoriesSideMenu,
-    },
-  };
-}
 
 const History = (categoriesSideMenu) => {
     return (
