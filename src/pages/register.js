03b6d7a8e350543f8bcca1bf0f2d037ad87317b7
François <81768287+guillermfr@users.noreply.github.com>
2023-04-25 13:52:37 +0200

    Update register.js

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,16 +1,6 @@
 import NavSign from '../components/NavSign'
 import RegisterForm from '../components/RegisterForm'
 
-import { getCategorieIdData } from '../fonctions/SidebarData';
-export async function getStaticProps() {
-    const categoriesSideMenu = await getCategorieIdData();
-    return {
-        props: {
-            categoriesSideMenu,
-        },
-    };
-}
-
 export default function RegisterPage(){
     return (
         <main className="flex flex-col items-center">
