09cadb38c3b4cdfc8b0590aefb6726b96c03b75f
François <81768287+guillermfr@users.noreply.github.com>
2023-04-25 13:17:57 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,7 +35,7 @@ export default function Layout({ children }) {
 
   return (  
     <>
-      <Nav childrenProps={children.props.categoriesSideMenu} />
+      <Nav />
           {children}  
       <Footer />
     </>
