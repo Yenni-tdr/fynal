acb0972574a6f29c82c35e07a61f4bc2ed5c7e31
Lucas <104208703+Venkkay@users.noreply.github.com>
2023-04-27 17:27:37 +0200

    Add files via upload

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,10 +1,9 @@
 import NavAccount from '../components/NavAccount'
-import RegisterForm from '../components/RegisterForm'
 import AccountBody from "../components/AccountBody";
 
 export default function AccountPage(){
     return (
-        <main className="flex flex-row flex-1">
+        <main className="flex max-sm:flex-col flex-1">
             <NavAccount />
             <AccountBody />
         </main>
